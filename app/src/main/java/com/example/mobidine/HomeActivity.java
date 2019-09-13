e01c81a291ff0c06704e8c8e966d4d3f3b24e70f
giwi97 <49786113+giwi97@users.noreply.github.com>
2019-09-13 18:21:24 +0530

    second commit

---
+++ app/src/main/java/com/example/mobidine/HomeActivity.java
@@ -0,0 +1,222 @@
+package com.example.mobidine;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.mobidine.Model.Products;
+import com.example.mobidine.Prevalent.Prevalent;
+import com.example.mobidine.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import io.paperdb.Paper;
+
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        Paper.init(this);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Home");
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView userNameTextView = headerView.findViewById(R.id.user_profile_name);
+        CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
+
+        userNameTextView.setText(Prevalent.currentonlineUser.getName());
+
+
+        //display pro pic on app drawre
+
+        Picasso.get().load(Prevalent.currentonlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
+
+
+        recyclerView = findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>().setQuery(ProductsRef, Products.class).build();
+
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter=
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+
+                    }
+
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i, @NonNull Products products) {
+
+                        productViewHolder.txtProductName.setText(products.getName());
+                        productViewHolder.txtProductDescription.setText(products.getDescription());
+                        productViewHolder.txtProductPrice.setText("Price:"+products.getPrice()+"LKR");
+                        Picasso.get().load(products.getImage()).into(productViewHolder.imageView);
+
+
+
+                    }
+                };
+
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+          //  return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_cart) {
+
+        } else if (id == R.id.nav_orders) {
+
+        } else if (id == R.id.nav_categories) {
+
+        } else if (id == R.id.nav_settings) {
+
+            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(intent);
+
+
+
+        } else if (id == R.id.nav_logout) {
+
+            Paper.book().destroy();
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/mobidine/SettingsActivity.java
@@ -0,0 +1,290 @@
+package com.example.mobidine;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mobidine.Prevalent.Prevalent;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private CircleImageView profileImageView;
+    private EditText fullNameEditText, userPhoneEditText, addressEditText;
+    private TextView profileChangeTextButton, closeBtn, saveTextButton;
+
+
+    private Uri imageUri;
+    private String myUrl = "";
+    private StorageTask uploadTask;
+    private StorageReference storageReference;
+    private String checker = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        profileImageView = (CircleImageView) findViewById(R.id.settings_profile_image);
+
+        fullNameEditText = (EditText) findViewById(R.id.settings_full_name);
+        userPhoneEditText = (EditText) findViewById(R.id.settings_phone_number);
+        addressEditText = (EditText) findViewById(R.id.settings_address);
+
+        profileChangeTextButton = (TextView) findViewById(R.id.profile_image_change_btn);
+        closeBtn = (TextView) findViewById(R.id.close_settings_btn);
+        saveTextButton = (TextView) findViewById(R.id.update_account_settings_btn);
+
+
+        userInfoDisplay(profileImageView, fullNameEditText, userPhoneEditText, addressEditText);
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+
+            }
+        });
+
+        saveTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(checker.equals("clicked")){
+
+                    userInfoSaved();
+                }else{
+
+                    updateOnlyUserInfo();
+
+
+                }
+
+            }
+        });
+
+
+        profileChangeTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                checker = "clicked";
+
+                //Once user open the image gallery to select image, crop image window will open
+
+                CropImage.activity(imageUri).setAspectRatio(1, 1)
+                        .start(SettingsActivity.this);
+
+
+            }
+        });
+        
+    }
+
+    private void updateOnlyUserInfo() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        HashMap<String, Object> userMap = new HashMap<>();
+        userMap.put("name", fullNameEditText.getText().toString());
+        userMap.put("address", addressEditText.getText().toString());
+        userMap.put("phoneOrder", userPhoneEditText.getText().toString());
+
+        ref.child(Prevalent.currentonlineUser.getPhone()).updateChildren(userMap);
+
+        startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+        Toast.makeText(SettingsActivity.this, "Profile updated successfully..", Toast.LENGTH_SHORT).show();
+        finish();
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null){
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            profileImageView.setImageURI(imageUri);
+        }else{
+
+            Toast.makeText(this, "Error occured!!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(SettingsActivity.this, SettingsActivity.class));
+
+            finish();
+
+        }
+
+    }
+
+    private void userInfoSaved() {
+
+        if(TextUtils.isEmpty(fullNameEditText.getText().toString())){
+
+            Toast.makeText(this, "Name is required", Toast.LENGTH_SHORT).show();
+
+        }else if(TextUtils.isEmpty(userPhoneEditText.getText().toString())){
+
+            Toast.makeText(this, "Phone number is required", Toast.LENGTH_SHORT).show();
+
+        }else if(TextUtils.isEmpty(addressEditText.getText().toString())){
+
+            Toast.makeText(this, "Address is required", Toast.LENGTH_SHORT).show();
+
+        }else if(checker.equals("clicked")){
+
+            uploadImage();
+        }
+
+    }
+
+    private void uploadImage() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Update Profile");
+        progressDialog.setMessage("Please while, we're setting things..");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        if(imageUri != null){
+
+            final  StorageReference fileRef = storageReference.child(Prevalent.currentonlineUser.getPhone() + ".jpg");
+
+            uploadTask = fileRef.putFile(imageUri);
+
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+
+                    if (!task.isSuccessful()){
+
+                        throw task.getException();
+
+                    }
+
+
+
+                    return fileRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+
+                    if(task.isSuccessful()){
+
+                        Uri downloadUrl = task.getResult();
+
+                        myUrl = downloadUrl.toString();
+
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap.put("name", fullNameEditText.getText().toString());
+                        userMap.put("address", addressEditText.getText().toString());
+                        userMap.put("phoneOrder", userPhoneEditText.getText().toString());
+                        userMap.put("image", myUrl);
+
+                        ref.child(Prevalent.currentonlineUser.getPhone()).updateChildren(userMap);
+
+
+
+                        progressDialog.dismiss();
+
+
+                        startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+                        Toast.makeText(SettingsActivity.this, "Profile updated successfully..", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }else {
+
+                        progressDialog.dismiss();
+                        Toast.makeText(SettingsActivity.this, "Error occured", Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                }
+            });
+
+        }else {
+
+            Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show();
+
+
+
+        }
+
+    }
+
+
+    private void userInfoDisplay(final CircleImageView profileImageView, final EditText fullNameEditText, final EditText userPhoneEditText, final EditText addressEditText) {
+
+        DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(Prevalent.currentonlineUser.getPhone());
+
+        UsersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if ((dataSnapshot.exists())){
+
+                    if(dataSnapshot.child("image").exists()){
+
+                        String image = dataSnapshot.child("image").getValue().toString();
+                        String name = dataSnapshot.child("name").getValue().toString();
+                        String phone = dataSnapshot.child("phone").getValue().toString();
+                        String address = dataSnapshot.child("address").getValue().toString();
+
+
+                        Picasso.get().load(image).into(profileImageView);
+                        fullNameEditText.setText(name);
+                        userPhoneEditText.setText(phone);
+                        addressEditText.setText(address);
+
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+}
