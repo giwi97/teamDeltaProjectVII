c1f492bb3bf7e5bc9e0e7ffd1710f5967343f291
giwi97 <49786113+giwi97@users.noreply.github.com>
2019-09-15 09:22:22 +0530

    second commit

--- app/src/main/java/com/example/mobidine/AdminMaintainProductsActivity.java
+++ app/src/main/java/com/example/mobidine/AdminMaintainProductsActivity.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 
 public class AdminMaintainProductsActivity extends AppCompatActivity {
 
-    private Button applyChangesBtn;
+    private Button applyChangesBtn, deleteBtn;
     private EditText name, price, description;
     private ImageView imageView;
 
@@ -48,6 +48,7 @@ public class AdminMaintainProductsActivity extends AppCompatActivity {
         price = findViewById(R.id.product_price_maintain);
         description = findViewById(R.id.product_description_maintain);
         imageView = findViewById(R.id.product_image_maintain);
+        deleteBtn = findViewById(R.id.delete_product_btn);
 
 
 
@@ -59,6 +60,34 @@ public class AdminMaintainProductsActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 applyChanges();
+
+            }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                deleteThisProduct();
+
+            }
+        });
+
+
+    }
+
+    private void deleteThisProduct() {
+
+        productsRef.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                Toast.makeText(AdminMaintainProductsActivity.this, "Successfully delete", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(AdminMaintainProductsActivity.this, AdminCategoryActivity.class);
+                startActivity(intent);
+                finish();
+
 
             }
         });
